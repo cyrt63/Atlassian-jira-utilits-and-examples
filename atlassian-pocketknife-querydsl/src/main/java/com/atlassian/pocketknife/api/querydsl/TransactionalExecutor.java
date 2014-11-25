0e5d4dfd7b8e737e4e9426b4a15af257cf12c4fc
Adam Hynes <ahynes@atlassian.com>
2014-11-25 18:26:13 +1100

    TFCP-334: Added extra descriptions to Javadoc.

--- atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/api/querydsl/TransactionalExecutor.java
+++ atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/api/querydsl/TransactionalExecutor.java
@@ -15,18 +15,19 @@ import javax.annotation.Nonnull;
 public interface TransactionalExecutor
 {
     /**
-     * Executes the provided {@code function} within a new transaction.
+     * Executes the provided {@code function} within a new transaction. Connection retrieval is taken care of
+     * implicitly.
      * <p/>
-     * The transaction will be committed immediately after the function completes.
-     * <p/>
-     * The transaction will be rolled back if an exception is thrown during the execution of the function.
+     * The transaction will be committed immediately after the function completes. The transaction will be rolled back
+     * if an exception is thrown during the execution of the function.
      * <p/>
      * The {@link java.sql.Connection} is provided to the function for convenience (for example, to allow the function
      * to commit in between operations).
      *
      * @param function what will be executed in a new transaction
      * @param <T> the return type of the function, may be {@link java.lang.Void}
-     * @throws java.lang.RuntimeException on failure to commit or roll back the transaction
+     * @throws java.lang.RuntimeException on failure to commit or roll back the transaction, or if the supplied
+     *         {@code function} throws an exception
      * @return the result of the function
      */
     <T> T executeInTransaction(@Nonnull Function<Connection, T> function);
