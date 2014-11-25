4ad1813b1e6befcb8c9f63e3f679e3ede02f0f33
Adam Hynes <ahynes@atlassian.com>
2014-11-25 18:16:14 +1100

    TFCP-334: Added TransactionalExecutor, which invokes functions within a database transaction, with connection/commit/rollback semantics included.

---
+++ atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/api/querydsl/TransactionalExecutor.java
@@ -0,0 +1,33 @@
+package com.atlassian.pocketknife.api.querydsl;
+
+import com.atlassian.annotations.PublicApi;
+import com.google.common.base.Function;
+
+import java.sql.Connection;
+import javax.annotation.Nonnull;
+
+/**
+ * Provides functionality to execute {@link com.google.common.base.Function}s within a database transaction, with
+ * connection retrieval, commit and rollback operations taken care of.
+ *
+ */
+@PublicApi
+public interface TransactionalExecutor
+{
+    /**
+     * Executes the provided {@code function} within a new transaction.
+     * <p/>
+     * The transaction will be committed immediately after the function completes.
+     * <p/>
+     * The transaction will be rolled back if an exception is thrown during the execution of the function.
+     * <p/>
+     * The {@link java.sql.Connection} is provided to the function for convenience (for example, to allow the function
+     * to commit in between operations.
+     *
+     * @param function what will be executed in a new transaction
+     * @param <T> the return type of the function, may be {@link java.lang.Void}
+     * @throws java.lang.RuntimeException on failure to commit or roll back the transaction
+     * @return the result of the function
+     */
+    <T> T executeInTransaction(@Nonnull Function<Connection, T> function);
+}
