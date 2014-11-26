e0b7a882a89d8bc641c5e1f21d24f6e23ff7d567
Adam Hynes <ahynes@atlassian.com>
2014-11-26 16:19:40 +1100

    TFCP-334: LoggingSqlListener now logs all types of SQL operations, including insert, update, delete and batch operations.

--- atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/api/querydsl/LoggingSqlListener.java
+++ atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/api/querydsl/LoggingSqlListener.java
@@ -18,10 +18,10 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+
 /**
- * A logger implementation you can use to cause QueryDSL SQL messages to be logged as they are executed
- * <p/>
- * NOTE : This does not log all the values on the batch methods.
+ * A logger implementation you can use to cause QueryDSL SQL messages to be logged as they are executed.
  */
 @PublicApi
 public class LoggingSqlListener implements SQLListener
@@ -35,23 +35,15 @@ public class LoggingSqlListener implements SQLListener
         this.configuration = configuration;
     }
 
-    private void log(final RelationalPath<?> entity, final QueryMetadata md)
+    private void log(final RelationalPath<?> entity, final String sql)
     {
         StringBuilder sb = new StringBuilder();
-        if (entity != null)
+        if (entity != null && isNotBlank(entity.getTableName()) && isNotBlank(sql))
         {
-            sb.append(entity.getTableName());
+            sb.append("Executing query on ").append(entity.getTableName()).append(" table: ");
         }
-        if (sb.length() > 0)
+        if(isNotBlank(sql))
         {
-            sb.append(" ");
-        }
-        if (md != null)
-        {
-            SQLSerializer sqlSerializer = new SQLSerializer(configuration);
-            sqlSerializer.setUseLiterals(true);
-            sqlSerializer.serialize(md, false);
-            String sql = sqlSerializer.toString();
             sb.append(sql);
         }
         if (sb.length() > 0)
@@ -63,54 +55,143 @@ public class LoggingSqlListener implements SQLListener
     @Override
     public void notifyQuery(final QueryMetadata md)
     {
-        log(null, md);
+        SQLSerializer serializer = newLiteralPrintingSerializer();
+        serializer.serialize(md, false);
+        log(null, serializer.toString());
     }
 
     @Override
     public void notifyDelete(final RelationalPath<?> entity, final QueryMetadata md)
     {
-        log(entity, md);
+        log(entity, getDeleteSql(entity, md));
     }
 
     @Override
     public void notifyDeletes(final RelationalPath<?> entity, final List<QueryMetadata> batches)
     {
-        log(entity, null);
+        log(entity, getBatchDeleteSql(entity, batches));
     }
 
     @Override
     public void notifyMerge(final RelationalPath<?> entity, final QueryMetadata md, final List<Path<?>> keys, final List<Path<?>> columns, final List<Expression<?>> values, final SubQueryExpression<?> subQuery)
     {
-        log(entity, md);
+        log(entity, getMergeSql(entity, md, keys, columns, values, subQuery));
     }
 
     @Override
     public void notifyMerges(final RelationalPath<?> entity, final QueryMetadata md, final List<SQLMergeBatch> batches)
     {
-        log(entity, md);
+        log(entity, getBatchMergeSql(entity, md, batches));
     }
 
     @Override
     public void notifyInsert(final RelationalPath<?> entity, final QueryMetadata md, final List<Path<?>> columns, final List<Expression<?>> values, final SubQueryExpression<?> subQuery)
     {
-        log(entity, md);
+        log(entity, getInsertSql(entity, md, columns, values, subQuery));
     }
 
     @Override
     public void notifyInserts(final RelationalPath<?> entity, final QueryMetadata md, final List<SQLInsertBatch> batches)
     {
-        log(entity, md);
+        log(entity, getBatchInsertSql(entity, md, batches));
     }
 
     @Override
     public void notifyUpdate(final RelationalPath<?> entity, final QueryMetadata md, final List<Pair<Path<?>, Expression<?>>> updates)
     {
-        log(entity, md);
+        log(entity, getUpdateSql(entity, md, updates));
     }
 
     @Override
     public void notifyUpdates(final RelationalPath<?> entity, final List<SQLUpdateBatch> batches)
     {
-        log(entity, null);
+        log(entity, getBatchUpdateSql(entity, batches));
+    }
+
+    private String getDeleteSql(final RelationalPath<?> entity, final QueryMetadata queryMetadata)
+    {
+        SQLSerializer serializer = newLiteralPrintingSerializer();
+        serializer.serializeDelete(queryMetadata, entity);
+        return serializer.toString();
+    }
+
+    private String getBatchDeleteSql(final RelationalPath<?> entity, final List<QueryMetadata> deleteQueriesMetadata)
+    {
+        StringBuilder batchDeleteQueries = new StringBuilder();
+        batchDeleteQueries.append("<Start batch delete> ");
+        for(QueryMetadata queryMetadata : deleteQueriesMetadata)
+        {
+            batchDeleteQueries.append(getDeleteSql(entity, queryMetadata));
+            batchDeleteQueries.append("; ");
+        }
+        batchDeleteQueries.append(" <End batch delete>");
+        return batchDeleteQueries.toString();
+    }
+
+    private String getMergeSql(final RelationalPath<?> entity, final QueryMetadata md, final List<Path<?>> keys, final List<Path<?>> columns, final List<Expression<?>> values, final SubQueryExpression<?> subQuery)
+    {
+        SQLSerializer serializer = newLiteralPrintingSerializer();
+        serializer.serializeMerge(md, entity, keys, columns, values, subQuery);
+        return serializer.toString();
+    }
+
+    private String getBatchMergeSql(final RelationalPath<?> entity, final QueryMetadata md, final List<SQLMergeBatch> mergeBatches)
+    {
+        StringBuilder batchMergeQueries = new StringBuilder();
+        batchMergeQueries.append("<Start batch merge> ");
+        for(SQLMergeBatch mergeBatch : mergeBatches)
+        {
+            batchMergeQueries.append(getMergeSql(entity, md, mergeBatch.getKeys(), mergeBatch.getColumns(), mergeBatch.getValues(), mergeBatch.getSubQuery()));
+            batchMergeQueries.append("; ");
+        }
+        batchMergeQueries.append(" <End batch merge>");
+        return batchMergeQueries.toString();
+    }
+
+    private String getInsertSql(final RelationalPath<?> entity, final QueryMetadata md, final List<Path<?>> columns, final List<Expression<?>> values, final SubQueryExpression<?> subQuery)
+    {
+        SQLSerializer serializer = newLiteralPrintingSerializer();
+        serializer.serializeInsert(md, entity, columns, values, subQuery);
+        return serializer.toString();
+    }
+
+    private String getBatchInsertSql(final RelationalPath<?> entity, final QueryMetadata md, final List<SQLInsertBatch> insertBatches)
+    {
+        StringBuilder batchInsertQueries = new StringBuilder();
+        batchInsertQueries.append("<Start batch insert> ");
+        for(SQLInsertBatch insertBatch : insertBatches)
+        {
+            batchInsertQueries.append(getInsertSql(entity, md, insertBatch.getColumns(), insertBatch.getValues(), insertBatch.getSubQuery()));
+            batchInsertQueries.append("; ");
+        }
+        batchInsertQueries.append(" <End batch insert>");
+        return batchInsertQueries.toString();
+    }
+
+    private String getUpdateSql(final RelationalPath<?> entity, final QueryMetadata md, final List<Pair<Path<?>, Expression<?>>> updates)
+    {
+        SQLSerializer serializer = newLiteralPrintingSerializer();
+        serializer.serializeUpdate(md, entity, updates);
+        return serializer.toString();
+    }
+
+    private String getBatchUpdateSql(final RelationalPath<?> entity, final List<SQLUpdateBatch> updateBatches)
+    {
+        StringBuilder batchUpdateQueries = new StringBuilder();
+        batchUpdateQueries.append("<Start batch update> ");
+        for(SQLUpdateBatch updateBatch : updateBatches)
+        {
+            batchUpdateQueries.append(getUpdateSql(entity, updateBatch.getMetadata(), updateBatch.getUpdates()));
+            batchUpdateQueries.append("; ");
+        }
+        batchUpdateQueries.append(" <End batch update>");
+        return batchUpdateQueries.toString();
+    }
+
+    private SQLSerializer newLiteralPrintingSerializer()
+    {
+        SQLSerializer literalPrintingSerializer = new SQLSerializer(configuration);
+        literalPrintingSerializer.setUseLiterals(true);
+        return literalPrintingSerializer;
     }
 }
