cc65d17b419f09dff6462fd84535ee4e11f7ae96
Adam Hynes <ahynes@atlassian.com>
2014-12-03 08:25:26 +1100

    POCKET-7: Renamed to EnhancedRelationalPathBase to better reflect it's new role with additional functionality.

--- atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/spi/querydsl/EnhancedRelationalPathBase.java
+++ atlassian-pocketknife-querydsl/src/main/java/com/atlassian/pocketknife/spi/querydsl/EnhancedRelationalPathBase.java
@@ -5,14 +5,21 @@ import com.mysema.query.sql.RelationalPathBase;
 import com.mysema.query.sql.SchemaAndTable;
 
 /**
- * * QueryDSL table classes that extend this class can be instantiated without specifying a schema.
+ * Enhances {@link RelationalPathBase} with additional functionality designed to make writing and executing queries
+ * using table entities that extend this class easier.
  * <p/>
- * This allows them to be declared as static variables and used at call sites and yet still have the capability of
- * knowing the underlying schema name which is not known until runtime.
+ * This extra functionality includes:
+ * <ul>
+ *     <li>
+ *         QueryDSL table classes that extend this class can be instantiated without specifying a schema. This allows
+ *         them to be declared as static variables and used at call sites and yet still have the capability of
+ *         knowing the underlying schema name which is not known until runtime.
+ *     </li>
+ * </ul>
  */
-public abstract class RelationalBasePathWithDynamicSchema<T> extends RelationalPathBase<T>
+public abstract class EnhancedRelationalPathBase<T> extends RelationalPathBase<T>
 {
-    public RelationalBasePathWithDynamicSchema(
+    public EnhancedRelationalPathBase(
             final Class<? extends T> type,
             final String tableName)
     {
