ff394e27a98dbdec1ee1c5d99695b4b14a6f2853
Michael Ruflin <miruflin@atlassian.com>
2015-01-28 15:01:42 +0700

    Revert "SDR-59: Removing migrator that uses a star select query due to AO-552 which removed support for this. Breaks API, I know, but the original code simply blows up in JIRA 7.0 so you have to fix your code either way"
    This reverts commit d8874fb6ff32c13af2fcdb739f7ed94dc5b3d3ff.

--- jira-pocketknife-upgradetask/src/main/java/com/atlassian/pocketknife/api/ao/util/AoStreamingMigrator.java
+++ jira-pocketknife-upgradetask/src/main/java/com/atlassian/pocketknife/api/ao/util/AoStreamingMigrator.java
@@ -14,25 +14,21 @@ import net.java.ao.Query;
 public abstract class AoStreamingMigrator<E extends Entity>
 {
     private final Class<E> classOfEntity;
+    private final ActiveObjects ao;
 
-    private ActiveObjects ao;
-    private Query query;
     private long read;
     private long written;
 
-    /**
-     * Usage: Create a new AoStreaemingMigrator anonymous class, implement required methods and call
-     * migrate(ao) on it.
-     */
-    public AoStreamingMigrator(Class<E> classOfEntity)
+    public AoStreamingMigrator(Class<E> classOfEntity, ActiveObjects ao)
     {
-        this.classOfEntity = classOfEntity;
+        this(classOfEntity, Query.select("*"), ao);
     }
 
-    public final void migrate(ActiveObjects ao) {
+    public AoStreamingMigrator(Class<E> classOfEntity, Query query, ActiveObjects ao)
+    {
+        this.classOfEntity = classOfEntity;
         this.ao = ao;
 
-        Query query = buildQuery();
         ao.stream(classOfEntity, query, new EntityStreamCallback<E, Integer>()
         {
             @Override
@@ -42,9 +38,8 @@ public abstract class AoStreamingMigrator<E extends Entity>
             }
         });
         onEnd();
-
-        this.ao = null;
     }
+
 
     /**
      * Gets a writeable version of the read only entity by re-reading it off the database
@@ -75,15 +70,6 @@ public abstract class AoStreamingMigrator<E extends Entity>
      * @param readOnlyE a streamed READ ONLY version of the entity.  You MUST get a writeable
      */
     protected abstract void onRowRead(E readOnlyE);
-
-    /**
-     * You need to implement this method
-     *
-     * @return the query to run. Originally this defaulted to Query.select("*"), but this isn't supported anymore,
-     *         so you will have to spell out each individual field you want to query. For more infos see
-     *         https://ecosystem.atlassian.net/browse/AO-552
-     */
-    protected abstract Query buildQuery();
 
     /**
      * A template method allowing you to do something once all rows have been migrated
