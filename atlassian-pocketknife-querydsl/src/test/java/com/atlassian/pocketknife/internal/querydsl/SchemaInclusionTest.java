f5f9aca12f746e3a4705d241e1189c531e3f8b72
Ben Morgan <bmorgan@atlassian.com>
2015-04-21 09:54:03 +1000

    Demonstrates that the schema name is not included when using pocket knife resulting in potentiall undefined behave if you configure your database in a perverse manner

--- atlassian-pocketknife-querydsl/src/test/java/com/atlassian/pocketknife/internal/querydsl/SchemaInclusionTest.java
+++ atlassian-pocketknife-querydsl/src/test/java/com/atlassian/pocketknife/internal/querydsl/SchemaInclusionTest.java
@@ -7,10 +7,14 @@ import com.atlassian.pocketknife.api.querydsl.QueryFactory;
 import com.atlassian.pocketknife.spi.querydsl.AbstractConnectionProvider;
 import com.atlassian.pocketknife.spi.querydsl.DefaultDialectConfiguration;
 import com.mysema.commons.lang.CloseableIterator;
+import com.mysema.query.sql.Configuration;
+import com.mysema.query.sql.HSQLDBTemplates;
 import com.mysema.query.sql.RelationalPathBase;
 import com.mysema.query.sql.SQLQuery;
+import com.mysema.query.sql.SQLTemplates;
 import com.mysema.query.types.path.NumberPath;
 import com.mysema.query.types.path.StringPath;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,55 +32,76 @@ public class SchemaInclusionTest
     private DialectProvider dialectProvider;
 
     @Before
-    public void setup() throws SQLException
-    {
-        try
-        {
-            Class clazz = Class.forName("org.h2.Driver");
-        }
-        catch (Exception e)
-        {
-            System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
-            e.printStackTrace();
-            return;
-        }
-
-        connection = DriverManager.getConnection("jdbc:h2:mem:test");
-
-        connectionProvider = new AbstractConnectionProvider()
-        {
-            @Override
-            protected Connection getConnectionImpl(final boolean autoCommit)
-            {
-                return connection;
-            }
-        };
-        dialectProvider = new DefaultDialectConfiguration(connectionProvider);
-
-        queryFactory = new QueryFactoryImpl(connectionProvider, dialectProvider);
+    public void setup() throws SQLException{
+//    {
+//        try
+//        {
+//            Class clazz = Class.forName("org.h2.Driver");
+//        }
+//        catch (Exception e)
+//        {
+//            System.err.println("ERROR: failed to load HSQLDB JDBC driver.");
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        connection = DriverManager.getConnection("jdbc:h2:mem:test");
+//
+//        connectionProvider = new AbstractConnectionProvider()
+//        {
+//            @Override
+//            protected Connection getConnectionImpl(final boolean autoCommit)
+//            {
+//                return connection;
+//            }
+//        };
+//        dialectProvider = new DefaultDialectConfiguration(connectionProvider);
+//
+//        queryFactory = new QueryFactoryImpl(connectionProvider, dialectProvider);
     }
 
     @Test
     public void testSchemaStuff() throws SQLException
     {
-        System.out.println(connection.isClosed());
+        String schema = "HELLO";
+        String otherSchema = "BYE";
+
+        connectionProvider = new TestConnectionProvider(schema);
+        dialectProvider = new DefaultDialectConfiguration(connectionProvider);
+
+        queryFactory = new QueryFactoryImpl(connectionProvider, dialectProvider);
+
+        connection = connectionProvider.borrowConnection();
+
         final Statement statement = connection.createStatement();
-        statement.execute("create table FOO (ID IDENTITY, NAME VARCHAR2(64))");
-        statement.execute("insert into FOO (NAME) values ('hello')");
-//        connection.commit();
-//
-//        connection = DriverManager.getConnection("jdbc:h2:mem:test");
-//        connection.
+        statement.execute("create table FOO (ID INTEGER, NAME VARCHAR(64))");
+        statement.execute("insert into FOO (ID, NAME) values (1, 'this is my HELLO name')");
 
-        System.out.println(connection.isClosed());
-        final SQLQuery query = queryFactory.select(connection);
+        statement.execute("create schema BYE AUTHORIZATION DBA\n"
+                + " create table FOO (ID INTEGER, NAME VARCHAR(64))");
 
-        final QFooMapping mapping = new QFooMapping("fm", "", "FOO");
-        final CloseableIterator<String> iteratee = query.from(mapping).iterate(mapping.NAME);
-        while (iteratee.hasNext())
-        {
-            System.out.println(iteratee.next());
-        }
+        statement.execute("insert into BYE.FOO (ID, NAME) values (1, 'this is my BYE name')");
+
+        final SQLTemplates template = HSQLDBTemplates.builder().printSchema().build();
+        final SQLQuery query = new SQLQuery(connection, template);
+
+        final QFooMapping mapping = new QFooMapping("fm", otherSchema, "FOO");
+        final SQLQuery queryFrom = query.from(mapping);
+        final CloseableIterator<String> iteratee = queryFrom.iterate(mapping.NAME);
+
+        // This shows the schema being included
+        System.out.println(queryFrom.getSQL(mapping.NAME).getSQL());
+
+        Assert.assertEquals("this is my BYE name", iteratee.next());
+
+        final SQLQuery pocketknifeQuery = queryFactory.select(connection).from(mapping);
+
+        // This shows the schema being excluded
+        System.out.println(pocketknifeQuery.getSQL(mapping.NAME).getSQL());
+
+        final CloseableIterator<String> pocketIteratee = pocketknifeQuery.iterate(mapping.NAME);
+
+        Assert.assertEquals("this is my BYE name", pocketIteratee.next());
     }
 
     private class QFooMapping extends RelationalPathBase<QFooMapping>
