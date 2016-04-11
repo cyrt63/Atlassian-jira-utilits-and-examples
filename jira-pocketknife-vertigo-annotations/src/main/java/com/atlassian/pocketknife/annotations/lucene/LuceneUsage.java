adc7096db0dbbeb5b0cadc46564478ab5317f3f6
Andrew Swan <aswan@atlassian.com>
2016-04-12 08:32:19 +1000

    VI-1275 Added IssueSearchExtractor as a Lucene usage type

--- jira-pocketknife-vertigo-annotations/src/main/java/com/atlassian/pocketknife/annotations/lucene/LuceneUsage.java
+++ jira-pocketknife-vertigo-annotations/src/main/java/com/atlassian/pocketknife/annotations/lucene/LuceneUsage.java
@@ -1,7 +1,7 @@
 package com.atlassian.pocketknife.annotations.lucene;
 
 /**
- * Annotation for marking a class as using parts of lucene.
+ * Annotation for marking a class as using parts of Lucene.
  *
  * @since 0.72.0
  */
@@ -11,12 +11,18 @@ public @interface LuceneUsage {
 
     String comment() default "";
 
-    // Whether diced oxen should ignore this usage for the purposes of generating vertigo incompatibilities. Set to true
-    // when the code will use lucene post-vertigo or where the code only exists to constrain remaining lucene usage (but
-    // will be the last piece of lucene usage to go away.
+    /**
+     * Whether diced oxen should ignore this usage for the purposes of generating Vertigo incompatibilities. Set to true
+     * when the code will use Lucene post-Vertigo or where the code only exists to constrain remaining Lucene usage (but
+     * will be the last piece of Lucene usage to go away.
+     *
+     * @return false by default
+     */
     boolean ignore() default false;
 
-    // The type of lucene usage
+    /**
+     * The type of Lucene usage.
+     */
     enum LuceneUsageType {
         // Collector classification
         IssueCollection,
@@ -26,6 +32,7 @@ public @interface LuceneUsage {
         IssuePropertyCollection,
         IssuePropertyAggregation,
         Indexer,
+        IssueSearchExtractor,
 
         // Catch-all, use {@link #comment()} to elaborate why it doesn't fit other categories or add a type here.
         Unknown
